ed1cc6dee46cec7b53a2b0bcedb719184ce1ff4f
Jailson Leocadio <jailsonleocadio@MacBook-Air-de-Jailson.local>
2021-02-16 21:26:46 -0300

    removed the libraries install in the app.R file

--- dashboard/app.R
+++ dashboard/app.R
@@ -2,38 +2,13 @@
 # jailsonleocadio@gmail.com
 # Beekeep team (http://beekeep.life)
 
-if (!require(shiny)) {
-  install.packages("shiny")
-  library(shiny)
-}
-
-if (!require(shinydashboard)) {
-  install.packages("shinydashboard")
-  library(shinydashboard)
-}
-
-if (!require(leaflet)) {
-  install.packages("leaflet")
-  library(leaflet)
-}
-
-if (!require(ggplot2)) {
-  install.packages("ggplot2")
-  library(ggplot2)
-}
-
-if (!require(devtools)) {
-  install.packages("devtools")
-  library(devtools)
-}
-
-if (!require(plotly)) {
-  devtools::install_github("ropensci/plotly")
-  library(plotly)
-}
+library(shiny)
+library(shinydashboard)
+library(leaflet)
+library(plotly)
 
 source("database/database.R")
-source("functions/server.R")
 source("ui/ui.R")
+source("functions/server.R")
 
 shinyApp(ui = ui, server = server)
